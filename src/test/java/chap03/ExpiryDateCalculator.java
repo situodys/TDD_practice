9b6ed2ac26340ebbba1e5897a75d70d6bbf31507
situodys <situodys@gmail.com>
2022-07-07 15:52:13 +0900

    Refactor: ExpiryDateCalculator

--- src/test/java/chap03/ExpiryDateCalculator.java
+++ src/test/java/chap03/ExpiryDateCalculator.java
@@ -1,20 +1,38 @@
 package chap03;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 import java.time.temporal.ChronoUnit;
 
 public class ExpiryDateCalculator {
     public LocalDate calculateExpiryDate(PayData payData) {
-        if (payData.getFirstPayDate() != null) {
-            LocalDate candidateExp = payData.getPayDate().plusMonths(1);
-            if (payData.getFirstPayDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
-                return candidateExp.withDayOfMonth(payData.getFirstPayDate().getDayOfMonth());
-            }
-            if (payData.getFirstPayDate().equals(LocalDate.of(2019, 1, 31))) {
-                return LocalDate.of(2019, 3, 31);
-            }
-        }
 
-        return payData.getPayDate().plusMonths(1);
+        int addedMonths = payData.getPayAmount()/10_000;
+        if (payData.getFirstPayDate() != null) {
+            return expiryDateUsingFirstPayDate(payData, addedMonths);
+        }else{
+            return payData.getPayDate().plusMonths(addedMonths);
+        }
+    }
+
+    private LocalDate expiryDateUsingFirstPayDate(PayData payData, int addedMonths) {
+        LocalDate candidateExp = payData.getPayDate().plusMonths(addedMonths);
+        final int dayOfFirstPay = payData.getFirstPayDate().getDayOfMonth();
+        if (isSameDayOfMonth(candidateExp, dayOfFirstPay)) {
+            return candidateExp;
+        }
+        return getCandidateDateWithModifiedDay(candidateExp, dayOfFirstPay);
+    }
+
+    private boolean isSameDayOfMonth(LocalDate candidateExp, int dayOfFirstPay) {
+        return dayOfFirstPay == candidateExp.getDayOfMonth();
+    }
+
+    private LocalDate getCandidateDateWithModifiedDay(LocalDate candidateExp, int dayOfFirstPay) {
+        final int dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth();
+        if(dayLenOfCandiMon < dayOfFirstPay){
+            return candidateExp.withDayOfMonth(dayLenOfCandiMon);
+        }
+        return candidateExp.withDayOfMonth(dayOfFirstPay);
     }
 }
